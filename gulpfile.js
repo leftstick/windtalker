fe39def558bc87222063e681ca51adfbbde813f1
Howard.Zuo <leftstick@qq.com>
2015-01-04 17:49:50 +0800

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,18 @@
 
 var gulp = require('gulp');
 
-gulp.task('release', function() {
+gulp.task('release', function () {
     var NwBuilder = require('node-webkit-builder');
     var nw = new NwBuilder({
         files: ['./css/**/*.*', './fonts/**/*.*', './img/**/*.*', './js/**/*.*', './node_modules/**/*.*', './index.html', './package.json'],
         version: 'v0.11.4',
-        winIco: './img/favicon.png',
         macZip: true,
         platforms: ['osx32', 'osx64', 'win32', 'win64']
     });
 
-    nw.build().then(function() {
+    nw.build().then(function () {
         console.log('all done!');
-    }).catch(function(error) {
+    }).catch(function (error) {
         console.error(error);
     });
 });

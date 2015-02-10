f186fe1084b15b2dfce1e93e1c4d0ad91b9b06e8
Howard.Zuo <leftstick@qq.com>
2015-02-10 17:51:49 +0800

    add confirm feature

--- src/js/features/secret/controller/InfoController.js
+++ src/js/features/secret/controller/InfoController.js
@@ -11,16 +11,26 @@
 
     define([], function() {
 
-        var InfoController = function($scope) {
+        var InfoController = function($scope, events) {
 
             $scope.infos = [{
                 name: 'http://www.baidu.com',
                 desc: '百度的小玩意'
             }];
 
+            $scope.removeInfo = function(info) {
+                events.emit('alert', {
+                    message: '确定要删除选中信息么？',
+                    type: 'confirm',
+                    onClose: function() {
+                        console.log(arguments);
+                    }
+                });
+            };
+
         };
 
-        return ['$scope', InfoController];
+        return ['$scope', 'events', InfoController];
 
     });
 

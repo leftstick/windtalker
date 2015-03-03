976864f91690fcc5030b3f3c2c6a199a8acd7b8a
Howard.Zuo <leftstick@qq.com>
2015-03-03 11:27:29 +0800

    adjust gulpfile for different build

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,14 @@ var SYSTEMS = {
     win32: 'win'
 };
 
-gulp.task('less', function() {
+var devBuild = !require('minimist')(process.argv.slice(2)).prod;
+
+gulp.task('clean', function(cb) {
+    var rimraf = require('rimraf');
+    rimraf('build/', cb);
+});
+
+gulp.task('less', ['clean'], function() {
     var less = require('gulp-less');
     var LessPluginAutoPrefix = require('less-plugin-autoprefix');
     var autoprefix = new LessPluginAutoPrefix({
@@ -22,19 +29,34 @@ gulp.task('less', function() {
         .pipe(gulp.dest('src/css/'));
 });
 
-gulp.task('install', function() {
+gulp.task('gen-pkg', function() {
+    var template = require('gulp-template');
+    var rename = require('gulp-rename');
+    return gulp.src('src/package.json_vm')
+        .pipe(template({
+            devBuild: devBuild
+        }))
+        .pipe(rename({
+            extname: '.json'
+        }))
+        .pipe(gulp.dest('src/'));
+});
+
+gulp.task('install', ['less', 'gen-pkg'], function() {
     var install = require('gulp-install');
     return gulp.src(['./bower.json', './src/package.json'])
         .pipe(install());
 });
 
-gulp.task('release', ['less', 'install'], function() {
+gulp.task('default', ['install'], function() {
     var NwBuilder = require('node-webkit-builder');
+
+    var platforms = devBuild ? [SYSTEMS[os.platform()] + os.arch().substring(1)] : ['osx64', 'win64'];
     var nw = new NwBuilder({
         files: ['./src/css/**/*.*', './src/fonts/**/*.*', './src/img/**/*.*', './src/js/**/*.*', './src/node_modules/**/*.*', './src/index.html', './src/package.json'],
         version: 'v0.11.6',
         macZip: true,
-        platforms: [SYSTEMS[os.platform()] + os.arch().substring(1)]
+        platforms: platforms
             // platforms: ['osx32', 'osx64', 'win32', 'win64']
     });
 
